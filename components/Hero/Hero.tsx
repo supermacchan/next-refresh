167293286ad2d775288cca26655e1ddd5c39c568
Olga Birkovska <birkovska.dev@gmail.com>
2024-10-26 13:41:25 +0300

    hero upd

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className='pt-[76px]'>
-        <div className='min-h-screen grid grid-cols-2'>
-            <div className='relative overflow-hidden after:content-[""] after:absolute after:inset-0 after:bg-black/30'>
+    <section className='pt-[76px] min-h-screen'>
+        <div className=' grid grid-cols-2 h-full'>
+            <div className='relative min-h-full overflow-hidden after:content-[""] after:absolute after:inset-0 after:bg-black/30'>
                 <Image 
                     src="/images/home/hero/hero-img.jpg" 
                     alt="hero" 
-                    width={500} 
-                    height={500} 
+                    width={800} 
+                    height={1000}
                     className='object-cover w-full h-full'
                 />
             </div>
