9966001ea89877efc79fb825fbf88ed214fef646
Olga Birkovska <birkovska.dev@gmail.com>
2024-10-26 12:16:20 +0300

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import { Josefin_Sans } from 'next/font/google';
+import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const josefinSans = Josefin_Sans({
+  subsets: ['latin'],
+  variable: '--font-josefin-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={josefinSans.className}>{children}</body>
     </html>
   );
 }
